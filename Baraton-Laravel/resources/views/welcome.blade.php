f8d3b37c1fc00e431cabddfd4b00e5ec6d58d962
kipt002ke <98545605+kipt002ke@users.noreply.github.com>
2022-06-28 12:12:00 +0100

    google maps

--- Baraton-Laravel/resources/views/welcome.blade.php
+++ Baraton-Laravel/resources/views/welcome.blade.php
@@ -473,7 +473,7 @@
 
 							</ul>
 			</div>
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3074.7905052320443!2d-77.84987248482734!3d39.586871613613056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c9f6a80ccf0661%3A0x7210426c67abc40!2sVirginia+Welcome+Center%2FSafety+Rest+Area!5e0!3m2!1sen!2sin!4v1485760915662" ></iframe>
+			<iframe src="https://mapcarta.com/12746886"></iframe>
 		</div>
 		<div class="clearfix"></div>
 	</div>
